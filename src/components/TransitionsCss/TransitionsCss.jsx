79472f78ddc0e9e35c869b0a1b16120f9ce5b44e
leonelborgesdev <leonel03t@gmail.com>
2024-02-27 08:40:34 -0400

    transitions

--- src/components/TransitionsCss/TransitionsCss.jsx
+++ src/components/TransitionsCss/TransitionsCss.jsx
@@ -3,11 +3,9 @@ import "./TransitionsCss.css";
 
 export const TransitionsCss = () => {
   return (
-    <div className="container_transitions">
-      <div className="texts">
-        <h2 className="name item">Leonel Borges</h2>
-        <h3 className="paragraph item">CSS3 Transitions</h3>
-      </div>
+    <div className="texts">
+      <h2 className="name item">Leonel Borges</h2>
+      <h3 className="paragraph item">CSS3 Transitions paragraph</h3>
     </div>
   );
 };
